0ede3114d3db278e2a7eba0f2e419de07240f3bf
yogeshrithe <yogeshrithe.yr@gmail.com>
2022-12-05 16:25:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='no_wrap'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore, fuga, officiis numquam molestiae vitae quis qui delectus, possimus hic minima incidunt culpa nam molestias architecto amet ipsam nemo. Corporis?</h1>
+    </div>
+  )
+}
+
+export default App
